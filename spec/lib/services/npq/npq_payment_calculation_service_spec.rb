af463505248aff19e3fcc1ed031d21d7815bb65c
Pavel Lisovin <pavel.lisovin@digital.education.gov.uk>
2021-04-07 13:32:43 +0100

    Refactor few rspec test for BigDecimal and Numeric values

--- spec/lib/services/npq/npq_payment_calculation_service_spec.rb
+++ spec/lib/services/npq/npq_payment_calculation_service_spec.rb
@@ -3,21 +3,49 @@
 QUALIFICATION_NAME = "foo"
 
 describe NpqPaymentCalculationService do
-  before do
-    @config = {
+  let(:config) do
+    {
       recruitment_target: 2000,
       price_per_participant: BigDecimal(456.78, 10),
       number_of_monthly_payments: 19,
+      retention_points: {
+        "first": {
+          retained_participants: 100,
+          percentage: 20,
+        },
+        "second": {
+          retained_participants: 70,
+          percentage: 20,
+        },
+        "completion": {
+          retained_participants: 70,
+          percentage: 20,
+        },
+      },
     }
-    calculator = NpqPaymentCalculationService.new(@config)
-    @result = calculator.service_fee_schedule
   end
 
-  it "returns BigDecimal for all money outputs" do
-    expect(@result.dig(:output, :service_fee_payment_schedule, 1)).to be_a(BigDecimal)
+  describe "#service_fee_schedule" do
+    let(:result) { NpqPaymentCalculationService.new(config).service_fee_schedule }
+
+    it "includes config in the output" do
+      expect(result[:input]).to eq(config)
+    end
+
+    it "returns BigDecimal for all money outputs" do
+      result.dig(:output, :service_fee_payment_schedule).each do |_key, value|
+        expect(value).to be_a(BigDecimal)
+      end
+    end
   end
 
-  it "includes config in the output" do
-    expect(@result[:input]).to eq(@config)
+  describe "#variable_fee_schedule" do
+    let(:result) { NpqPaymentCalculationService.new(config).variable_fee_schedule }
+
+    it "returns Number for all other numeric outputs" do
+      result.dig(:output, :variable_fee_schedule).each do |_key, value|
+        expect(value[:total_variable_fee]).to be_a(Numeric)
+      end
+    end
   end
 end
